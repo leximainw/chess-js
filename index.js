7d061fcec48f0e6e0b80af64a1059a05ce511a5b
Alexis Mainwaring <leximainw@gmail.com>
2022-07-16 15:45:27 -0400

    implement drag-and-drop on chess pieces

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const boardGrid = document.querySelector("main .board");
 const gameState = new ChessBoard();
 init();
 
+// source: https://stackoverflow.com/questions/36532307/rem-px-in-javascript
+function convertRemToPx(rem)
+{
+    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
+
 function drawBoard(board)
 {
-    for (let piece of board.pieces)
+    for (const piece of board.pieces)
     {
         let pieceImg = document.querySelector(`#piece${piece.id}`);
         if (pieceImg === null)
@@ -12,7 +18,69 @@ function drawBoard(board)
             pieceImg = document.createElement("img");
             pieceImg.id = `piece${piece.id}`;
             pieceImg.src = `images/${piece.piece.replace(" ", "-")}.svg`;
-            pieceImg.classList.add("piece")
+            pieceImg.classList.add("piece");
+
+            pieceImg.draggable = false;
+            const dragState = {};
+            pieceImg.addEventListener("mousedown", e =>
+            {
+                dragState.startX = e.x;
+                dragState.startY = e.y;
+                dragState.dragging = false;
+                const moveHandler = e =>
+                {
+                    const dx = e.x - dragState.startX;
+                    const dy = e.y - dragState.startY;
+                    const dist2 = dx * dx + dy * dy;
+                    if (Math.sqrt(dist2) > convertRemToPx(0.25))
+                    {
+                        if (!dragState.dragging)
+                        {
+                            dragState.dragging = true;
+                            const rect = pieceImg.getBoundingClientRect();
+                            dragState.left = rect.left + window.screenX;
+                            dragState.top = rect.top + window.screenY;
+                            dragState.parent = pieceImg.parentElement;
+                            pieceImg.style.width = `${pieceImg.clientWidth}px`;
+                            pieceImg.style.height = `${pieceImg.clientHeight}px`;
+                            pieceImg.classList.add("dragging");
+                            document.body.prepend(pieceImg);
+                        }
+                        pieceImg.style.left = `${dragState.left + dx}px`;
+                        pieceImg.style.top = `${dragState.top + dy}px`;
+                    }
+                };
+                document.addEventListener("mousemove", moveHandler);
+                document.addEventListener("mouseup", e =>
+                {
+                    dragState.endX = e.x;
+                    dragState.endY = e.y;
+                    if (dragState.dragging)
+                    {
+                        const imgRect = pieceImg.getBoundingClientRect();
+                        const imgX = (imgRect.left + imgRect.right) / 2;
+                        const imgY = (imgRect.top + imgRect.bottom) / 2;
+                        for (const tile of boardGrid.children)
+                        {
+                            const rect = tile.getBoundingClientRect();
+                            if (imgX >= rect.left && imgY >= rect.top
+                                && imgX < rect.right && imgY < rect.bottom)
+                            {
+                                dragState.parent = tile;
+                                break;
+                            }
+                        }
+                        const newTile = dragState.parent;
+                        while (newTile.firstChild)
+                            newTile.removeChild(newTile.firstChild);
+                        pieceImg.classList.remove("dragging");
+                        piece.style = null;
+                        newTile.appendChild(pieceImg);
+                    }
+                    document.removeEventListener("mousemove", moveHandler);
+                },
+                { once: true });
+            });
         }
         const index = piece.position.x + 56 - piece.position.y * 8;
         boardGrid.children[index].appendChild(pieceImg);
